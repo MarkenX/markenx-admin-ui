3a9a4b01c914f7fc74a0700142ab2fd05cefe939
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-19 02:52:38 -0500

    feat: Se agrega configuración del proxy para evitar errores de CORS

--- src/app/core/api/core/OpenAPI.ts
+++ src/app/core/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8082',
+    BASE: '',
     VERSION: '0.0.1-SNAPSHOT',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
